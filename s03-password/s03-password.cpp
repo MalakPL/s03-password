50ab50d9332e059020ca0d41f6bd65d606b6aff2
Malak <szmajdakarol@gmail.com>
2021-10-24 20:42:21 +0200

    Dodaj pliki projektów.

---
+++ s03-password/s03-password.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <string>
+
+using std::string;
+using std::logic_error;
+using std::cout;
+using std::cin;
+using std::cerr;
+using std::endl;
+
+auto main(int argc, const char* argv[]) -> int
+{
+	try
+	{
+		if (argc != 2)
+		{
+			throw logic_error{ "Nie podano argumentu" };
+		}
+
+		string Password{};
+		do
+		{
+			cin >> Password;
+		} while(Password != argv[1]);
+
+		cout << "ok!" << endl;
+
+		return 0;
+	}
+	catch (logic_error ex)
+	{
+		cerr << ex.what() << endl;
+		return 1;
+	}
+}
